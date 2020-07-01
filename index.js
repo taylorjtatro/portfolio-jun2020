ab21dbbd8136ecdd61ac7c1adff4b781c11577cd
taylorjtatro <taylorjtatro@gmail.com>
2020-06-30 22:44:11 -0700

    working on contact section

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ let modals = document.querySelectorAll('.modal');
 let projectsModal = document.querySelector('#projects-section');
 let aboutModal = document.querySelector('#about-section');
 //let x = document.querySelector('.x');
-
+let contactModal = document.querySelector('#contact-section');
 
 
 let projectButton = document.querySelector('.projectButton');
 let aboutButton = document.querySelector('.aboutButton');
 
+let contactButton = document.querySelector('.contactButton');
 
 projectButton.onclick = function() {
     projectsModal.style.display = 'block';
@@ -17,6 +18,11 @@ projectButton.onclick = function() {
 aboutButton.onclick = function() {
     aboutModal.style.display = 'block';
 }
+
+contactButton.onclick = function() {
+    contactModal.style.display = 'block';
+}
+
 
 //x.onclick = function() {
 //    modal.style.display = 'none';
